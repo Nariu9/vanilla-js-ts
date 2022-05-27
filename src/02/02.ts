4bc9bd17b007438b1997e3fcd5c9ee5bcb0d0d0f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-27 15:41:26 +0300

    add objects, testing objects

---
+++ src/02/02.ts
@@ -0,0 +1,46 @@
+type LocalCityType = {
+    title:string
+    countryTitle: string
+}
+
+type LocalAddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechnologiesType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: LocalAddressType
+    technologies: Array<TechnologiesType>
+}
+
+
+const student: StudentType = {
+    id: 1,
+    name: "Artem",
+    age: 32,
+    isActive: true,
+    address: {
+        streetTitle: "Central Str",
+        city: {
+            title: "Maison",
+            countryTitle: "Uran"
+        }
+    },
+    technologies: [
+        {id:1, title: "HTML"},
+        {id:2, title: "CSS"},
+        {id:3, title: "React"},
+
+    ]
+}
+
+console.log(student.technologies[2].title)
